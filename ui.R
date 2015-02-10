0b50685a812f3d9dfc74789240d835d2d0fd98db
tomjw <tjwnt5@gmail.com>
2015-02-10 11:46:59 +1100

    changed layout, sidebar to right

--- ui.R
+++ ui.R
@@ -1,29 +1,20 @@
 library(shiny)
 
-## Cosmicflows-2 catalog
-
-# Define UI  application that plots random distributions 
-shinyUI(pageWithSidebar(
+shinyUI(fluidPage(
         
-        # Application title
-        headerPanel("Determine Hubble constant from Galaxy Data"),
+        titlePanel("Determine Hubble constant from Galaxy Data"),
         
-        
-        
-        # Sidebar with a slider input for number of observations
-        sidebarPanel(
-                sliderInput("obs", 
-                            "Number of Galaxies:", 
-                            min = 10,
-                            max = length(galaxy$GV), 
-                            value = 100,
-                            step = 10)
-        ),
-        
-        # Show a plot of the generated distribution
-        mainPanel(
+        sidebarLayout(position = "right",
                 
-                        p('This application uses the distance and recession velocity for 8315 galaxies, compiled by Tulley et al. (2013). 
+                sidebarPanel(
+                        sliderInput("obs", 
+                                    "Number of Galaxies:", 
+                                    min = 10,
+                                    max = length(galaxy$GV), 
+                                    value = 100,
+                                    step = 10)
+                ),
+                mainPanel(p('This application uses the distance and recession velocity for 8315 galaxies, compiled by Tulley et al. (2013). 
                           The independently determined distances and recession velocities are plotted and a regression line determined for
                           the resulting relationship.  The slope of the line gives the value of the Hubble Constant.'),
                         p('The Hubble Constant is a measure of the expansion rate of the universe, the value of which helps determine the overall geometry of the 
@@ -32,9 +23,10 @@ shinyUI(pageWithSidebar(
                           The complete dataset is re-sampled for each selection.  Refreshing the page also re-samples the dataset, using 100 of the
                         galaxies.'),
                         p('Reference:'), em( 'Tully R. B. et al., 2013, AJ, 146, 86'),
-                
-                plotOutput("galaxyPlot"),
-                verbatimTextOutput("summary")
-        ) 
-        
+                        
+                        plotOutput("galaxyPlot"),
+                        verbatimTextOutput("summary")
+                )
+        )
 ))
+
